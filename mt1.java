e30b5cf357005644c4abeb596a4ee7025b1e0b21
sahrudhai <121444666+sahrudhai@users.noreply.github.com>
2023-07-03 11:54:13 +0530

    Add files via upload

---
+++ mt1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/*class demo1{
+    void fun1() throws Exception
+    {
+        System.out.println("Addition task started");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter num2: ");
+        int num2 = sc.nextInt();
+        int num = num1 + num2;
+        System.out.println(num);
+    }
+}*/
+
+class demo2{
+    void fun2() throws Exception
+    {
+        System.out.println("Character Printing Started");
+        for(int i = 0; i <= 15; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
+public class mt1 {
+    public static void main(String args[]) throws Exception
+    {
+        demo1 d1 = new demo1();
+        demo2 d2 = new demo2();
+        d1.fun1();
+        d2.fun2();
+    }
+}
